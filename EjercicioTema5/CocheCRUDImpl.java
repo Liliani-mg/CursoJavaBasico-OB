0f455f9e4e8d1be8155564e457085066fbc76f89
Irupe <lilianirupemeza@gmail.com>
2023-02-07 08:57:58 -0300

    Ejercicios tema5 interfaces

---
+++ EjercicioTema5/CocheCRUDImpl.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CocheCRUDImpl implements CocheCRUD {
+
+    private List<Coche> coches = new ArrayList<>();
+
+    @Override
+    public void save(Coche coche) {
+
+        coches.add(coche);
+
+        System.out.println("Método save");
+    }
+
+    @Override
+    public List<Coche> findAll() {
+
+        System.out.println("Método findAll");
+
+        return coches;
+    }
+
+    @Override
+    public void delete(Coche coche) {
+
+        coches.remove(coche);
+        System.out.println("Método remove");
+
+
+    }
+
+}
